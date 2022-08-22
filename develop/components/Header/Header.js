3400ab068ed1b093c846e85cace14f14f930fb78
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-08-22 04:01:00 -0400

    finish styling header component for all screen sizes

--- develop/components/Header/Header.js
+++ develop/components/Header/Header.js
@@ -9,8 +9,44 @@ function define(html) {
     constructor() {
       super();
 
-      var shadow = this.attachShadow({ mode: 'open' });
+      const shadow = this.attachShadow({ mode: 'open' });
       shadow.innerHTML = html;
+
+      const header = this.shadowRoot.children[2];
+      const logo = this.shadowRoot.children[2].children[0];
+      const hero = this.shadowRoot.children[2].children[1];
+      const heroImage = this.shadowRoot.children[2].children[1].children[0];
+      const heroImageLeft = this.shadowRoot.children[2].children[1].children[1];
+      const heroImageRight =
+        this.shadowRoot.children[2].children[1].children[2];
+
+      this.observeScreen(heroImage, heroImageLeft, heroImageRight);
+    }
+
+    // VARIABLES
+    body = document.body;
+
+    // METHODS
+    observeScreen(...args) {
+      const [heroSmall, heroLeft, heroRight] = args;
+      console.log(heroSmall, heroLeft, heroRight);
+
+      const observer = new ResizeObserver((entries) => {
+        const bodyElement = entries[0];
+        const isDesktop = bodyElement.contentRect.width >= 1440;
+
+        if (isDesktop) {
+          heroSmall.classList.add('hide');
+          heroLeft.classList.remove('hide');
+          heroRight.classList.remove('hide');
+        } else {
+          heroSmall.classList.remove('hide');
+          heroLeft.classList.add('hide');
+          heroRight.classList.add('hide');
+        }
+      });
+
+      observer.observe(this.body);
     }
   }
   window.customElements.define('header-component', HeaderComponent);
